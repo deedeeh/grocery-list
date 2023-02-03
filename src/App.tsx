3a9b8a2d70c1b8e77d1fd649d934e55a8547b3fa
Dina Hafez <dina.hafez@guardian.co.uk>
2023-02-03 18:11:08 +0000

    Added a simple Form component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './styles/App.css';
+import { Form } from './components/Form';
 
 function App() {
   return (
@@ -7,6 +8,7 @@ function App() {
       <header className="App-header">
         <h1>Grocery List</h1>
       </header>
+      <Form />
     </div>
   );
 }
---
+++ src/components/Form.tsx
@@ -0,0 +1,39 @@
+import { useState } from "react"
+
+const defaultFormData = {
+    item: "",
+    price: 0
+}
+
+export const Form = () => {
+    const [formData, setFormData] = useState(defaultFormData);
+    const { item, price } = formData
+
+    const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setFormData(prevState => ({
+            ...prevState,
+            [e.target.id]: e.target.value
+        }));
+    }
+
+    const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        console.log(formData)
+
+        setFormData(defaultFormData);
+    }
+
+    return (
+        <form onSubmit={onSubmit}>
+            <label htmlFor="item">Item</label>
+            <input type="text" id="item" value={item} onChange={onChange} />
+            <br />
+            <br />
+            <label htmlFor="price">Price</label>
+            <input type="number" id="price" value={price} onChange={onChange} />
+            <br />
+            <br />
+            <button type="submit">Add</button>
+        </form>
+    );
+}
