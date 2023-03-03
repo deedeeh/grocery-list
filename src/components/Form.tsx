5583c077daad18be93240668f15a6d11c9aa657c
Dina Hafez <dina.hafez@guardian.co.uk>
2023-03-03 12:59:20 +0000

    Ran npm run format and npm run lint

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,45 +1,41 @@
-import { useState } from "react"
+import { useState } from 'react';
 
 interface FormFields {
-    item: string,
-    price: number
+  item: string;
+  price: number;
 }
 
 const defaultFormData: FormFields = {
-    item: "",
-    price: 0
-}
+  item: '',
+  price: 0
+};
 
 export const Form = () => {
-    const [formData, setFormData] = useState(defaultFormData);
-    const { item, price } = formData
+  const [formData, setFormData] = useState(defaultFormData);
+  const { item, price } = formData;
 
-    const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setFormData(prevState => 
-            ({ ...prevState,
-                [e.target.id]: e.target.value
-            })
-        )
-    }
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFormData((prevState) => ({ ...prevState, [e.target.id]: e.target.value }));
+  };
 
-    const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault()
-        console.log(formData)
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(formData);
 
-        setFormData(defaultFormData);
-    }
+    setFormData(defaultFormData);
+  };
 
-    return (
-        <form onSubmit={onSubmit}>
-            <label htmlFor="item">Item</label>
-            <input type="text" id="item" value={item} onChange={onChange} />
-            <br />
-            <br />
-            <label htmlFor="price">Price</label>
-            <input type="number" id="price" value={price} onChange={onChange} />
-            <br />
-            <br />
-            <button type="submit">Add</button>
-        </form>
-    );
-}
+  return (
+    <form onSubmit={onSubmit}>
+      <label htmlFor="item">Item</label>
+      <input type="text" id="item" value={item} onChange={onChange} />
+      <br />
+      <br />
+      <label htmlFor="price">Price</label>
+      <input type="number" id="price" value={price} onChange={onChange} />
+      <br />
+      <br />
+      <button type="submit">Add</button>
+    </form>
+  );
+};
