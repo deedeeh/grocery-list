12fd98a67a1ae6636a1f23b6d4d15884e5cb61d5
Dina Hafez <dina.hafez@guardian.co.uk>
2023-05-26 15:56:55 +0100

    Solved the CORS issue to post to the server

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -12,7 +12,21 @@ const defaultFormData: FormFields = {
 
 export const Form = () => {
   const [formData, setFormData] = useState(defaultFormData);
+  const [groceryItem, setGroceryItem] = useState(defaultFormData);
+
   const { item, price } = formData;
+
+  const postDataToServer = async () => {
+    const request = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(formData)
+    };
+    await fetch('/api/grocery-list/item', request)
+      .then((response) => response.json())
+      .then((data) => console.log(data))
+      .catch((err) => console.log(err));
+  };
 
   const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setFormData((prevState) => ({ ...prevState, [e.target.id]: e.target.value }));
@@ -24,9 +38,9 @@ export const Form = () => {
 
   const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    console.log(formData);
-
+    postDataToServer();
     setFormData(defaultFormData);
+    setGroceryItem(defaultFormData);
   };
 
   return (
